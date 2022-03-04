2de3b48d40c40df7ef8fb74a7b6b000dbc715140
vladotoka <vladimir.k.terziev@tutanota.com>
2022-03-04 15:35:16 +0200

    gitignore2

--- env/keys.js
+++ env/keys.js
@@ -1,6 +1,6 @@
 const keys = {
-firebaseWebAPIkey: 'AIzaSyDfglqfE24EpW7DWJmrSeJm8AlGM67TedI',
-test: 'gitignoretest'
+	firebaseWebAPIkey: 'AIzaSyDfglqfE24EpW7DWJmrSeJm8AlGM67TedI',
+	test: 'gitignoretest',
 };
 
 export default keys;
